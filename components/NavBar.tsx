af6c5804dee19520b9335eacf19b2311438daf57
Quintin720 <lisaon75@gmail.com>
2025-05-20 12:33:02 +0100

    Edit NavBar

--- components/NavBar.tsx
+++
@@ -1,7 +0,0 @@
-export default function NavBar() {
-  return (
-    <nav style={{ padding: '1rem', background: '#eee' }}>
-      <h3>NeuroConnect</h3>
-    </nav>
-  );
-}
---
+++ components/componentsNavBar.tsx
@@ -0,0 +1,7 @@
+export default function NavBar() {
+  return (
+    <nav style={{ padding: '1rem', backgroundColor: '#f5f5f5' }}>
+      <h3 style={{ margin: 0 }}>NeuroConnect</h3>
+    </nav>
+  );
+}
